c26806279d2edb31a7bd0464724a042d79bbba32
TejasMarthak <tejasmarthak1909@gmail.com>
2026-01-11 00:06:19 +0530

    11/1/26 - 1

---
+++ src/context/notes/NoteState.js
@@ -0,0 +1,20 @@
+import NoteContext from "./noteContext";
+import { useState } from "react";
+
+const NoteState = (props) => {
+  const s1 = {
+    name: "Tejas",
+    class: "5b",
+    };
+    const [state, setstate] = useState(s1);
+    const update = () => {
+        setTimeout(() => {
+            setstate({name: "CloudNotebook", class: "10b"})
+        }, 1000);
+    }
+  return (
+    <NoteContext.Provider value={{state, update}}>{props.children}</NoteContext.Provider>
+  );
+};
+
+export default NoteState;
